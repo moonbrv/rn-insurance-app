d1e1b20bc6e44fd5f6b57852da484ad4195023b8
Dmitry Palamarchuk <moonbrv@gmail.com>
2018-04-01 15:18:16 +0300

    add back button handling

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
-import { View, StatusBar } from 'react-native'
+import { View, StatusBar, BackHandler, Platform } from 'react-native'
 import ReduxNavigation from '../Navigation/ReduxNavigation'
 import { connect } from 'react-redux'
 import StartupActions from '../Redux/StartupRedux'
 import ReduxPersist from '../Config/ReduxPersist'
+import { NavigationActions } from 'react-navigation'
+import { path } from 'ramda'
 
 // Styles
 import styles from './Styles/RootContainerStyles'
 
 class RootContainer extends Component {
+  componentWillMount () {
+    if (Platform.OS === 'ios') return
+    BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackButton)
+  }
+
   componentDidMount () {
     this.props.startup()
+  }
+
+  onAndroidBackButton = () => {
+    const { currentKey, navigationIndex, goBack} = this.props
+    const shouldStayInApp = !!navigationIndex
+    if (shouldStayInApp) {
+      goBack(currentKey)
+    }
+    return shouldStayInApp
   }
 
   render () {
@@ -23,9 +39,18 @@ class RootContainer extends Component {
   }
 }
 
-// wraps dispatch to create nicer functions to call within our component
+const mapStateToProps = state => {
+  const currentIndex = path(['nav', 'index'], state)
+  const currentKey = path(['nav', 'routes', currentIndex, 'key'], state)
+  return {
+    navigationIndex: state.nav.index,
+    currentKey
+  }
+}
+
 const mapDispatchToProps = (dispatch) => ({
-  startup: () => dispatch(StartupActions.startup())
+  startup: () => dispatch(StartupActions.startup()),
+  goBack: key => dispatch(NavigationActions.back({ key }))
 })
 
-export default connect(null, mapDispatchToProps)(RootContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(RootContainer)
